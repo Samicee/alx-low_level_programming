ed53b8b8cd5f93f9f4d99e98f57950a4b52ed461
Samuel Mbah <samuelcmbah@gmail.com>
2022-12-30 14:43:39 -0800

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - Entry point
- *
- * Return: 0
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-/**
- * print_alphabet_x10 - entry
+ * print_alphabet - entry
  *
  * Return: (void)
  */
