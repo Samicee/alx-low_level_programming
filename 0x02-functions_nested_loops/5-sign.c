f0061d844803bef1c3ba106630b3ac18dbb445c7
Samuel Mbah <samuelcmbah@gmail.com>
2022-12-31 01:07:34 -0800

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,16 +10,16 @@ int print_sign(int n)
 	if (n < 0)
 	{
 		return (-1);
-		putchar('-' + '0');
+		_putchar('-');
 	}
 	else if (n == 0)
 	{
 		return (0);
-		putchar('0' + '0');
+		_putchar('0');
 	}
 	else
 	{
-		putchar('+' + '0');
+		_putchar('+');
 		return (1);
 	}
 }
