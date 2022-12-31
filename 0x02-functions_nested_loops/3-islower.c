9284ad246f3594cbd85521b7693bf0f600bd8c39
Samuel Mbah <samuelcmbah@gmail.com>
2022-12-30 22:31:55 -0800

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _islower - Entry
+ *
+ * Return: 1 || 0
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
