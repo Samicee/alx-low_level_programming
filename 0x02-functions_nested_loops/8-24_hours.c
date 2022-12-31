270d3f0588b43c8d99c81d930f60880cf16ea10e
Samuel Mbah <samuelcmbah@gmail.com>
2022-12-31 04:52:45 -0800

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,22 +6,26 @@
  */
 void jack_bauer(void)
 {
-	char i, j;
+	int i, j, x, y;
 
-	for (i = '0'; i <= '24'; i++)
+	for (i = 0; i <= 2; i++)
 	{
-		for (j = '0'; j <= '60'; j++)
+		for (j = 0; j <= 9; j++)
 		{
-			if (i < 10)
-				_putchar('0');
-			_putchar(i);
-			_putchar(' ');
-			_putchar(':');
-			_putchar(' ');
-			if (j < 10)
-				_putchar('0');
-			_putchar(j);
-			_putchar('\n');
+			for (x = 0; x <= 5; x++)
+			{
+				for (y = 0; y <= 9; y++)
+				{
+					_putchar(i + '0');
+					_putchar(j + '0');
+					_putchar(' ');
+					_putchar(':');
+					_putchar(' ');
+					_putchar(x + '0');
+					_putchar(y + '0');
+					_putchar('\n');
+				}
+			}
 		}
 	}
 }
