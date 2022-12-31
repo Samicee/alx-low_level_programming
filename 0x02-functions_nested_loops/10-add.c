0c0f01741aa69bff795b3d2cc3fb0f5d53dc5e7d
Samuel Mbah <samuelcmbah@gmail.com>
2022-12-31 07:36:12 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * add - fund the sum of two numbeers
+ * @x: first argument
+ * @y: second argument
+ *
+ * Return: int
+ */
+int add(int x, int y);
+{
+	return (x + y);
+}
