b260eb0356c9d79eefbdbb7ed2f1c28174a6c1d3
Samuel Mbah <samuelcmbah@gmail.com>
2023-01-02 09:23:28 -0800

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
  * print_line - draws a straight line in the terminal
+ * @n: argument
  */
 void print_line(int n)
 {
-	while(n > 0)
+	while (n > 0)
 	{
 		_putchar('_');
 		n--;
