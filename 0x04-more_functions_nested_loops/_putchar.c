0843593d9b175bd4c2bca4a194a23ea1b6ed83b1
Samuel Mbah <samuelcmbah@gmail.com>
2023-01-01 16:12:17 -0800

    print numbers

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _putchar - subs in char value
+ * @ch: argument
+ * Return: ch
+ */
+void _putchar(char ch)
+{
+	putchar(ch);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,5 @@
-char _putchar(char);
+void _putchar(char);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
